0ee5ce7e8f2c6a9afd68e4e824b24ebcfd9492e2
ro-neil <roneilbarrett@gmail.com>
2025-10-05 03:53:11 -0400

    adjusted search-input-container width for better mobile-first compatibility

--- src/components/utils/SearchInput.jsx
+++ src/components/utils/SearchInput.jsx
@@ -14,7 +14,7 @@ const SearchInput = ({ value, onChange, placeholder, id }) => {
             className="search-input-container"
             style={{
                 position: 'relative',
-                width: '38ch',
+                width: '36ch',
                 display: 'flex',
                 alignItems: 'center',
             }}
